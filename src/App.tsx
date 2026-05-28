e2d7d94fb28b1b889e91871a122c69e46a23b6ee
Asus TUF <honzahoangminhquan1234@gmail.com>
2026-05-29 01:06:24 +0200

    initial project setup

---
+++ src/App.tsx
@@ -0,0 +1,11 @@
+import './App.css'
+import CaseOpeningView from './features/Case/views/CaseOpeningView'
+
+function App() {
+
+    return (
+        <CaseOpeningView />
+    )
+}
+
+export default App
---
+++ src/features/Case/views/CaseOpeningView.tsx
@@ -0,0 +1,25 @@
+import React from 'react'
+
+export default function CaseOpeningView() {
+    return (
+        // Game
+        <div>
+            {/* Opening section */}
+            <section>
+                {/* Case Picture / Roulette */}
+
+                {/* Controls */}
+            </section>
+
+            {/* Case Contents */}
+            <section>
+                {/* Skin box */}
+            </section>
+
+            {/* Drop Odds */}
+            <section>
+                {/* Button opens Modal that will show odds */}
+            </section>
+        </div>
+    )
+}
